e7cede50db125a64b144eb01383f473de487cab5
yassinefathallah <92339390+yassinefathallah@users.noreply.github.com>
2023-12-05 13:32:11 +0100

    test 2

--- src/main/java/com/example/projet_securite/ProjetSecuriteApplication.java
+++ src/main/java/com/example/projet_securite/ProjetSecuriteApplication.java
@@ -8,6 +8,7 @@ public class ProjetSecuriteApplication {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello 222 ");
         SpringApplication.run(ProjetSecuriteApplication.class, args);
     }
 
